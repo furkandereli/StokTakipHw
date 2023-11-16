91efcb84a4276d285777d15f07a6df16387907e5
furkandereli <furkandereli98@gmail.com>
2023-11-16 15:16:39 +0300

    Techcareer bootcamp homework 2

---
+++ StokTakipHw/projects/TechCareerStokTakip/DataAccess/Repositories/Concrete/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using Core.Persistence.Repositories;
+using DataAccess.Context;
+using DataAccess.Repositories.Abstracts;
+using Models.Dtos.ResponseDto;
+using Models.Entities;
+
+
+namespace DataAccess.Repositories.Concrete
+{
+    public class ProductRepository : EfRepositoryBase<BaseDbContext, Product, Guid>, IProductRepository
+    {
+        public ProductRepository(BaseDbContext context) : base(context)
+        {
+        }
+
+        public List<ProductDetailDto> GetAllProductDetails()
+        {
+            var details = Context.Products.Join(
+                Context.Categories,
+                p => p.CategoryId,
+                c => c.Id,
+                (product, category) => new ProductDetailDto
+                {
+                    Name = product.Name,
+                    CategoryName = category.Name,
+                    Id = product.Id,
+                    Price = product.Price,
+                    Stock= product.Stock
+                }
+                ).ToList();
+
+            return details;
+        }
+
+        public List<ProductDetailDto> GetDetailsByCategoryId(int categoryId)
+        {
+            var values = Context.Products.
+                Where(p => p.CategoryId == categoryId)
+                .Join(
+                Context.Categories,
+                p => p.CategoryId,
+                c => c.Id,
+                (product, category) => new ProductDetailDto
+                {
+                    Name = product.Name,
+                    CategoryName = category.Name,
+                    Id = product.Id,
+                    Price = product.Price,
+                    Stock = product.Stock
+                }
+                ).ToList();
+
+            return values;
+        }
+
+        public ProductDetailDto GetProductDetail(Guid id)
+        {
+            var details = Context.Products
+                .Where(p => p.Id == id)
+                .Select(product => new ProductDetailDto
+                {
+                    Name = product.Name,
+                    CategoryName = product.Category.Name,
+                    Id = product.Id,
+                    Price = product.Price,
+                    Stock = product.Stock
+                }).FirstOrDefault();
+
+            return details;
+        }
+    }
+}
